9802cbc999ddcbd8e380cb890702be0882255332
yogesh123789 <yogesh.kumar@123789.org>
2025-04-28 23:49:49 +0530

    Initial commit

---
+++ scripts/__dropins__/tools/types/elsie/src/components/ImageSwatch/ImageSwatch.d.ts
@@ -0,0 +1,21 @@
+import { FunctionComponent, VNode } from 'preact';
+import { HTMLAttributes } from 'preact/compat';
+
+export interface ImageSwatchProps extends Omit<HTMLAttributes<HTMLInputElement>, 'label'> {
+    name?: string;
+    label?: string;
+    groupAriaLabel?: string;
+    id?: string;
+    src: string;
+    alt?: string;
+    value?: string;
+    disabled?: boolean;
+    selected?: boolean;
+    outOfStock?: boolean;
+    multi?: boolean;
+    imageNode?: VNode;
+    onValue?: (value: any) => void;
+    onUpdateError?: (error: Error) => void;
+}
+export declare const ImageSwatch: FunctionComponent<ImageSwatchProps>;
+//# sourceMappingURL=ImageSwatch.d.ts.map
